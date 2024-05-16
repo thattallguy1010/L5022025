8b8add0b5949c57f7aeabcdbd48a9450c430e48d
Tony Bourke <tony.bourke@sdn-pros.com>
2024-05-15 19:24:14 -0700

    Cleaning up

--- Python/fundamentals/JSON/get_mac.py
+++
@@ -1,9 +0,0 @@
-import pyeapi
-import json
-
-# Establish eAPI connection to leaf1
-connect = pyeapi.connect_to('leaf1')
-# Send command 'show mac address-table' to switch
-# The result is a dictionary (cmd_result) with dictionaries inside
-cmd_result = connect.enable(['show mac address-table', 'show ip interface brief'])
-print(cmd_result)
