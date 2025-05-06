f9f21165b6fa637b505d07dd71a9103ca43c7a15
Steven Forster <stevenforster10@gmail.com>
2025-05-06 04:21:11 +0000

    some changes

--- Ansible_CVP/python_scripts/get_dev_info.py
+++ Ansible_CVP/python_scripts/get_dev_info.py
@@ -2,7 +2,7 @@ from cvprac.cvp_client import CvpClient
 
 CVP_HOST = "192.168.0.5"
 CVP_USER = "arista"
-CVP_PWD = "arista5ufc"
+CVP_PWD = "aristaeffq"
 
 clnt = CvpClient()
 clnt.connect([CVP_HOST], CVP_USER, CVP_PWD, protocol='https')
